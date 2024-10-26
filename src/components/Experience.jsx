fae0b1a51e3bd564b1e54e5dc703f94153476cca
Abdullah al Tamim <79895300+abdullah-al-tamim@users.noreply.github.com>
2024-10-27 00:30:39 +0600

    bug: line not getting the animation
    The line between the experiences isn't showing up when using motion.div, but is visible when using div only

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,19 +2,34 @@
 import Heading from "./sub/Heading"
 import Image from 'next/image'
 import { experienceData, arrowLeftIcon } from "@/assets"
+import {motion , useScroll, useSpring} from 'framer-motion'
+import {useRef} from 'react'
 
 const Experience = () => {
     const date = new Date().getFullYear()
+
+    const containerRef = useRef(null)
+    const { scrollYProgress } = useRef({
+        target: containerRef,
+        offset: ['start 95%', 'end end'],
+    })
+
+    const scrollY = useSpring(scrollYProgress, {stiffness:200, damping:20})
+
     return (
         <div className="relative py-20 px-96">
             <Heading text={"Experience & Education"} />
             <Image src={"/education.png"} alt={"Experience image"} width={400} height={400} className="absolute -top-4 right-96 opacity-70 lg:hidden" />
 
-            <div className="w-full h-full flex flex-col items-center justify-center gap-y-10 lg:gap-y-20 py-10">
+            <div ref={containerRef} className="w-full h-full flex flex-col items-center justify-center gap-y-10 lg:gap-y-20 py-10">
                 {experienceData.map((data, i) => (
 
                     <div key={'id-${i}'} className={`w-[600px] xl:w-[480px] sm:w-full px-12 sm:px-0 relative ${i%2 === 0 ? "-left-[300px] xl:-left-[240px] lg:-left-0" : "left-[300px] xl:left-[240px] lg:left-0" }`}>
-                        <div className="relative flex flex-col gap-y-3 rounded-md border border-red-400 bg-white p-4 tracking-wide sm:text-sm">
+
+                        <motion.div initial={{opacity:0, x: i%2 === 0 ? -80 : 80}}  whileInView={{opacity:1, x:0, }} 
+                        viewport={{once:true}}
+                        transition={{duration: .7, type: "spring", stiffness:50}}
+                        className="relative flex flex-col gap-y-3 rounded-md border border-red-300 bg-white p-4 tracking-wide sm:text-sm">
                             <h1 className="text-xl sm:text-lg font-light text-gray-700">{data.title}</h1>
                             <p className="text-gray-800">
                                 <span className="block font-light">Education: </span>
@@ -30,14 +45,14 @@ const Experience = () => {
                             <span className={`absolute top-20  text-red-300 -translate-y-1/2 ${
                                 i%2 === 0 ? "left-full rotate-180" : "right-full"
                             }`}>{arrowLeftIcon}</span>
-                        </div>
+                        </motion.div>
                         <div className={`w-14 absolute top-20 border border-gray-300 rounded-full aspect-square grid place-items-center text-red-400 font-light -translate-y-1/2 z-10 bg-white ${i%2 === 0 ? "left-full -translate-x-1/2 lg:left-1/2" : "right-full translate-x-1/2 lg:right-1/2"}`}>
                             {date - experienceData.length + i + 1}
                         </div>
                     </div>
                 ))}
 
-                <div className="absolute w-1 h-full rounded-full bg-gray-300"></div>
+                <motion.div initial={{scaleY:0}} style={{scaleY: scrollY}} className="absolute w-1 h-full rounded-full bg-gray-600 origin-top"></motion.div>
             </div>
 
         </div>
