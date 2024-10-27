061dfbc7bec669e81a79593e6039e5e25d73ce19
Abdullah al Tamim <79895300+abdullah-al-tamim@users.noreply.github.com>
2024-10-27 21:50:36 +0600

    feat: Contact page done

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Experience from "@/components/Experience"
 import Skills  from "@/components/Skills"
 import { Reviews } from "@/components/Reviews"
 import Projects from "@/components/Projects"
+import Contact from '@/components/Contact'
 
 export default function Home() {
   return (
@@ -14,5 +15,6 @@ export default function Home() {
     <Experience/>
     <Skills />
     <Projects/>
+    <Contact />
   </div>)
 }
