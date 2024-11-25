31b2860b877d3dd5c7e7c2f88574de01144bedae
Abdullah al Tamim <79895300+abdullah-al-tamim@users.noreply.github.com>
2024-11-25 06:53:59 +0600

    Award page is updated

--- src/components/sub/Award.jsx
+++ src/components/sub/Award.jsx
@@ -14,7 +14,7 @@ const Award = ({ data, index }) => {
 
             <motion.div initial={{ opacity: 0 }} animate={{ opacity: show ? 1 : 0 }} whileInView={{ opacity: 1, y: 0 }}
                 className="relative flex flex-col gap-y-3 rounded-md border border-red-300 bg-white p-4 tracking-wide sm:text-sm dark:bg-zinc-700 transition-colors z-20">
-                <Image src={data.img} alt="Project Image" width={400} height={400} className="rounded-lg opacity-70" />
+                <Image src={data.img} alt="Project Image" width={100} height={100} className="rounded-lg opacity-100" />
                 <h1 className="text-xl sm:text-lg font-semibold text-gray-700 dark:text-white transition-colors">{data.title}</h1>
                 <p className="text-gray-800 dark:text-gray-100 transition-colors">
                     <span className="block font-bold">Institution: </span>
